fd6a5b9d9023ef70ef2e3491096e08e0bf543b84
JCDCuevas <jcdcuevas@gmail.com>
2019-08-23 23:47:56 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Redirect, withRouter, Switch } from 'react-router-dom';
+import DocumentTitle from 'react-document-title';
+import Topbar from './components/Topbar/Topbar';
+import Tableros from './screens/Tableros/Tableros';
+import Tables from './screens/Tables/Tables';
+import Summary from './screens/Summary/Summary';
+import Reportes from './screens/Reportes/Reportes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default withRouter(
+	class App extends React.PureComponent {
+		render() {
+			return (
+				<div>
+					<Topbar />
+					<Switch>
+						<RouteWithTitle exact title="Tableros" path="/tableros" component={Tableros} />
+						<RouteWithTitle exact title="Tablas" path="/tablas" component={Tables} />
+						<RouteWithTitle exact title="Resumen" path="/resumen" component={Summary} />
+						<RouteWithTitle exact title="Reportes" path="/reportes" component={Reportes} />
+						<Redirect to={'/inicio'} />
+					</Switch>
+				</div>
+			);
+		}z
+	}
+);
 
-export default App;
+export const RouteWithTitle = ({ title, render, component: Comp, ...props }) => (
+	<Route {...props} render={(p) => <DocumentTitle title={title}>{render ? render(p) : <Comp {...p} />}</DocumentTitle>} />
+);
